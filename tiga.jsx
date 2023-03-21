9e1fba6428ba7c531959bebb4e06bc09440b8e01
arumanwar09 <arumanwarsari@gmail.com>
2023-03-21 15:53:35 +0800

    src

---
+++ tiga.jsx
@@ -0,0 +1,24 @@
+function formatName(user) {
+    return user.firstName +  ' '  + user.lastName;
+}
+
+const user = {
+    firstName: 'arum' ,
+    lastName: 'sari'
+};
+
+const element = (
+    <h1>
+        Hello,  {formatName(user)}!
+    </h1>
+);
+
+
+function Tiga() (
+    return (
+        element
+    );
+)
+
+
+export default Tiga;
